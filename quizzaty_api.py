843fb6bae06406dbdcbe26c7a9b860b898a6fae9
Beshoy Bahaa <bahaa_beshoy@yahoo.com>
2025-05-22 03:42:29 +0300

    Update num_workers parameter in PropertyGraphIndex initialization of graphRAG class from 1 to 2 for improved processing efficiency and restore chunk_size parameter for clarity.

--- quizzaty_api.py
+++ quizzaty_api.py
@@ -144,8 +144,8 @@ class graphRAG:
             embed_model=self.embedding_model,
             storage_context=storage_context,  # Use the created storage context
             show_progress=True,
-            num_workers=1,
-            # chunk_size=1024,  # Process 1024 tokens per chunk
+            num_workers=2,
+            chunk_size=1024,  # Process 1024 tokens per chunk
             chunk_overlap=100,  # 300 token overlap between chunks
             chunk_sleep_time=90.0  # Sleep 1 second between chunks
         )
