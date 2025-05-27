a410dda75fa7a07662571bb70cab79b36f9dec45
Beshoy Bahaa <bahaa_beshoy@yahoo.com>
2025-05-27 22:55:56 +0300

    update model

--- quizzaty_api.py
+++ quizzaty_api.py
@@ -108,7 +108,7 @@ class graphRAG:
         # Configure Gemini with API key from environment variable or fallback to hardcoded key
 
         self.llm_gemini = Gemini(
-            model="gemini-1.5-flash",
+            model="Gemma-3",
             api_key="AIzaSyAwuVnbkTAMhR5-DxwYzwBN9-vilX_bnXY",
             max_retries=2
         )
