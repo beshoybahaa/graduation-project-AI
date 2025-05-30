e30d19ae3648642838d217d6143b21b662bce4dd
Beshoy Bahaa <bahaa_beshoy@yahoo.com>
2025-05-30 22:56:32 +0300

    make valiud uuid

--- final.py
+++ final.py
@@ -92,7 +92,7 @@ class graphRAG:
         from uuid import uuid4
 
         request_id = str(uuid4())
-        request_id = f"db_{str(raw_uuid).replace('-', '')}"
+        request_id = f"db_{str(request_id).replace('-', '')}"
 
         system_driver = GraphDatabase.driver(
                 "bolt://0.0.0.0:7687",
