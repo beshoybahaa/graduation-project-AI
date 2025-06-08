6704afba2ddee821d21111cf7d638388eb260554
Beshoy Bahaa <bahaa_beshoy@yahoo.com>
2025-06-08 22:52:02 +0300

    Remove auto-reload option from Uvicorn server in final.py

--- final.py
+++ final.py
@@ -460,7 +460,7 @@ async def predict(file: Annotated[UploadFile, File()],TOCBool = bool,chapters:Op
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
 #run server
     
